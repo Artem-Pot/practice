9fbb57934663b9243bee7cd9177366bfdbf176df
Artem <blbo@yandex.ru>
2023-03-06 16:35:57 +0400

    Добавил боковую панель с настройками
    Из настроек работает только смена тем, и то на половину.

--- scripts/script_messenger.js
+++ scripts/script_messenger.js
@@ -1,4 +1,4 @@
-import {botOneMessagesArr, botMessagesArr, ObText, botOneimgArr} from './script.chatarr.js';
+import {botMessagesArr, ObText, botOneimgArr} from './script.chatarr.js';
 
 const Message = document.querySelector('.main__correspondence'); // поле с сообщениями
 
@@ -16,78 +16,57 @@ userMessage.addEventListener("keypress", function(event) {
   }
 });
 
-//функция рандома для первого сообщений бота
-function getRandomIntInclusiveOne(min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(botOneMessagesArr.length-1);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-
-// //функция рандома для изображений бота
-// function getRandomIntInclusiveImg(min, max) {
-//     min = Math.ceil(0);
-//     max = Math.floor(botOneimgArr.length-1);
-//     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-// }
-
-//функция рандома для сообщений бота
+//функция рандома сообщений бота
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(0);
     max = Math.floor(botMessagesArr.length-1);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-//функция рандома для сообщений бота из массива объекта
+//функция для вывода ответа сообщений бота по упоминания фразы из массива
 function getRandomBotObj(min, max) {
     min = Math.ceil(0);
     max = Math.floor(3);                                      //максимальная длина массива 4, не получается изменить на ObText[letterСase].length
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-// ------------------первое сообщение и изображение бота, при загрузке страницы------------------
+// ------------------первое сообщение бота-----------------
 function textOneBot() { 
     const div = document.createElement('div');                                 // создание нового diva
-    div.className = "main__mess";                                            // присвоение новому элементу класс message__bot
+    div.className = "main__mess-system";                                            // присвоение новому элементу класс message__bot
     Message.append(div); // добавить блок с сообщениеем - последним
-    //время для сообщения
-    let d = new Date();
-    let h = `${d.getHours()}`.padStart(2, '0');
-    let m = `${d.getMinutes()}`.padStart(2, '0');
-    let displayDate = h + ":" + m;
+      //--------время для сообщения----------
+      const d = new Date();
+      const h = `${d.getHours()}`.padStart(2, '0');
+      const m = `${d.getMinutes()}`.padStart(2, '0');
+      const displayDate = h + ":" + m;
 
-    // создание первого рамдомного сообщения ботом
-    div.innerHTML = `<span class="main__text">${botOneMessagesArr[getRandomIntInclusiveOne()]}</span>
-    <div class="main__time-box">
+    // создание первого приветсвенного сообщения ботом
+    div.innerHTML = `<span class="main__text">Привет, я чат-бот и я умею:<br>
+    - Покажу все доступные команды - !список<br>
+    - Отвечать на фразы пользователя из базы, или в случае отсуствия - рандомной. Чтобы посмотреть все доступные фразы - !фраза<br>
+    - Покажу в сообщениях все мои используемые стикеры. Чтобы посмотреть стикеры - !стикер<br>
+    </span><div class="main__time-box">
         <span class="main__time">${displayDate}</span>
         <div class="main__read main__read-no"></div>
-    </div>`;
-            
+    </div>`;     
 }
 
-// рандомное изображение бота после первого сообщения
-// function imgOneBot() { 
-//     const div = document.createElement('div'); 
-//     div.className = "main__mess";
-//     Message.append(div);
-//     div.innerHTML = botOneimgArr[getRandomIntInclusiveImg()];
-// }
-
 setTimeout(() => { textOneBot(); }, 1000);   //вызов функции первого сообшения ботом через время после загрузки страницы
-//setTimeout(() => { imgOneBot(); }, 2000);  //вызов функции рамндомного изображения, после  первого сообшения ботом
 
 // -----------------сообщение пользвователя---------------------------
 function textInput() {         
     let userMessage = document.getElementById('main__input').value; // поиск текста в форме сообщения пользователя
     const div = document.createElement('div'); // создание нового diva
     const block = document.getElementById("placehere"); // прокрутка сообщения в блоке сообщений
-        //время для сообщения
-    let d = new Date();             
-    let h = `${d.getHours()}`.padStart(2, '0');
-    let m = `${d.getMinutes()}`.padStart(2, '0');
-    let displayDate = h + ":" + m;
+      //--------время для сообщения----------
+      const d = new Date();
+      const h = `${d.getHours()}`.padStart(2, '0');
+      const m = `${d.getMinutes()}`.padStart(2, '0');
+      var displayDate = h + ":" + m;
   
     if (userMessage === '') {
-        return;
+        document.getElementById("main__input").focus(); // фокусирует элемиент ввода сообщения
     }
 
     else if (userMessage === '!список') {
@@ -106,6 +85,7 @@ function textInput() {
         div.innerHTML = botOneimgArr; // выводит все стикеры
         div.className = "main__mess-system"; // присвоение новому элементу класс с системным сообщением
         document.getElementById('main__input').value = ''; // очистка блока сообщений
+        
     }
 
     else {
@@ -129,11 +109,11 @@ function textBot() {
     const userMessage = document.getElementById('main__input').value; // поиск текста в форме сообщения пользователя
     let letterСase = userMessage.trim().toLowerCase().replace(/[^а-яА-Яa-zA-Z ]/g, ""); // фильтр переводит сообщение в нижний регистр, исключает пробелы впередли и сзади, исключение всех символов кроме букв алфавита.
     const block = document.getElementById("placehere"); // прокрутка сообщения в блоке сообщений
-    //время для сообщения
-    let d = new Date();
-    let h = `${d.getHours()}`.padStart(2, '0');
-    let m = `${d.getMinutes()}`.padStart(2, '0');
-    let displayDate = h + ":" + m;
+      //--------время для сообщения----------
+      const d = new Date();
+      const h = `${d.getHours()}`.padStart(2, '0');
+      const m = `${d.getMinutes()}`.padStart(2, '0');
+      var displayDate = h + ":" + m;
 
      if (ObText[letterСase]) {                                                                       // поиск ответа в массиве на вопрос пользователя 'точная фраза'
             div.innerHTML = `<span class="main__text">${ObText[letterСase][getRandomBotObj()]}</span>
@@ -157,6 +137,15 @@ function textBot() {
     document.getElementById('main__input').value = ''; // очистка поля ввода от текста после отправки сообщения пользователем (работает не так как нужно т.к. когда она стояла в функции вывода сообщения пользователя она стила сообщение и я не мог взять от туда данные), не работает если просто указать переменную userMessage, поэтому пришлось искать элемент заново   
 }
 
+// -------------- Изменение светлой темы на тёмную.
+let submitDark = document.getElementById('button__dark'); // переменная - событие отправки, при нажати на кнопку
+submitDark.addEventListener('click', textBut); // обработчик события нажатии на кнопку
+
+function textBut() {
+    const root = document.querySelector(':root');
+    root.style.setProperty('--text-color-online', '#011627');
+}
+
 
 // добавить изменение галочки со временем
 // добавить ещё акаунтов пользователей в чате
@@ -165,10 +154,3 @@ function textBot() {
 // добавить функцию самообучения бота - добавить команду !команда, котое будет вызывать алерт в который добавить фразу и ответ к ней. 
 //Далее добавить, чтобы лог записывался и добавлялся в массив с новыми фразами, скорей всего не обойтись без сервера иначе после каждого обновления станицы всё будет пропадать. (node js и msql) или добавить куки для сохранения данных при перезагрузки страницы
 // добавить функцию поиска не только по точной фразе и но части предложения, чтоб увеличить словарный запас
-
-
-//  = `<span class="main__text">${ObText[letterСase][getRandomBotObj()]}</span>
-// <div class="main__time-box">
-//     <span class="main__time">18:12</span>
-//     <div class="main__read main__read-no"></div>
-// </div>`);
